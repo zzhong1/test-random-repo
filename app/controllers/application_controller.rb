0ed2a98d3e950b0ea9f0c85f57fc20785e757db3
zzhong1 <zhengzhon0@gmail.com>
2014-09-22 19:35:12 -0400

    add device_for in routes, add configure_permitted_parameters in Application Controller for sign in sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
+before_action :configure_permitted_parameters, if: :devise_controller?
 
-  decent_configuration do
-    strategy DecentExposure::StrongParametersStrategy
+
+  def configure_permitted_parameters
+	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit({ roles: [] }, :email, :password, :password_confirmation) }
+	devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :email, :remember_me) }
   end
-
-  protect_from_forgery with: :exception
+  
+ decent_configuration do
+	strategy DecentExposure::StrongParametersStrategy
+ end
+ 
+ protect_from_forgery with: :exception
 end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy, :create]
+   before_action :authenticate_user!, :admin?, except: [:index, :show]
+  #before_action :authenticate_user!, only: [:new, :edit, :update, :destroy, :create]
   
   expose(:categories)
   expose(:category)
@@ -43,5 +44,8 @@ class CategoriesController < ApplicationController
   private
     def category_params
       params.require(:category).permit(:name)
+    end
+    def admin?
+      redirect_to new_user_session_path unless current_user.admin?
     end
 end
