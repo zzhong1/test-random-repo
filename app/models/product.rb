cc46ee281a22f417273e055e214858f75c556f6f
zzhong1 <zhengzhon0@gmail.com>
2014-09-22 20:06:59 -0400

    solved models/product_spec

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,23 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+ 
+  validates :price, :format => { :with => /\A\d+(\.\d{1,2})?\z/}
+
+  def average_rating
+  	sum=0.0
+  	count=0.0
+  	self.reviews.each{|r|
+  		count=count+1
+  		sum+=r.rating
+  	}
+
+  count==0? 0 : sum/count
+  	
+ 
+
+  end
 end
