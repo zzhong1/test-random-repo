9c2baa10a0afbc542f3c2a9d8a32ba4eababa407
zzhong1 <zhengzhon0@gmail.com>
2014-09-23 04:21:07 -0400

    night commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_page/profile'
+
 devise_for :users, path: "auth", path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' } 
  resources :categories do
     resources :products do
@@ -7,4 +9,7 @@ devise_for :users, path: "auth", path_names: { sign_in: 'login', sign_out: 'logo
   end
 
   root 'categories#index'
+
+  get 'profile'  => 'static_page#profile'
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,69 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(firstname:  "AdminFirstName",
+			 lastname: "AdminLastName",
+             email: "admin123@example.com",
+             password:              "adminpass",
+             password_confirmation: "adminpass",
+             admin: true)
+
+5.times do |n|
+ 
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(firstname:  "UserFirstName",
+  			   lastname: "UserLastName",
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
+Category.create!(name: "Food")
+Category.create!(name: "Clothes")
+Product.create!(title: "Cake",
+				description: "best cake in the world",
+				price: 5.0,
+				category_id: 1,
+				user_id: 2)
+Product.create!(title: "Egg",
+				description: "best egg in the world",
+				price: 3.0,
+				category_id: 1,
+				user_id: 3)
+Product.create!(title: "Milk",
+				description: "best milk in the world",
+				price: 4.5,
+				category_id: 1,
+				user_id: 3)
+Product.create!(title: "hat",
+				description: "best hat in the world",
+				price: 10.0,
+				category_id: 2,
+				user_id: 4)
+Product.create!(title: "pants",
+				description: "best pants in the world",
+				price: 20.0,
+				category_id: 2,
+				user_id: 5)
+Product.create!(title: "sweater",
+				description: "best sweater in the world",
+				price: 15.5,
+				category_id: 2,
+				user_id: 6)
+Review.create!(content: "it was nice!",
+    		   rating:  4,
+               product_id: 1,
+    		   user_id: 3)
+
+Review.create!(content: "it was ok!",
+    		   rating:  3,
+               product_id: 1,
+    		   user_id: 4)
+Review.create!(content: "it was bad!",
+    		   rating:  1,
+               product_id: 1,
+    		   user_id: 5)
+Review.create!(content: "it was so so!",
+    		   rating:  2,
+               product_id: 1,
+    		   user_id: 6)
